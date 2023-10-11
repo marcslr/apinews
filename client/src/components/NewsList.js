2638bf8913453733e435886e5c88bf128335c21a
marcslr <sainthilaire.marcjoel@gmail.com>
2023-10-11 13:53:01 +0200

    Update NewsList.js

--- client/src/components/NewsList.js
+++ client/src/components/NewsList.js
@@ -5,19 +5,29 @@ import videoacc from "../videos/bg-acc.mp4";
 
 const NewsList = () => {
   const [articles, setArticles] = useState([]);
+  const [savedArticles, setSavedArticles] = useState([]);
 
   useEffect(() => {
     const getArticles = async () => {
-      // const apiKey = process.env.REACT_APP_NEWS_API_KEY;
-      const response = await axios.get(
-        `https://newsapi.org/v2/everything?q=france&apiKey=40fe80ab18514482872006fbd868527c`
-      );
-      setArticles(response.data.articles);
-      console.log(response);
+      try {
+        // Fetch news articles from an API
+        const response = await axios.get(
+          `https://newsapi.org/v2/everything?q=france&apiKey=40fe80ab18514482872006fbd868527c`
+        );
+        setArticles(response.data.articles);
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+      }
     };
 
     getArticles();
   }, []);
+
+  const handleSaveArticle = (article) => {
+    // Save the article to the user's account
+    // ...
+  };
 
   return (
     <div className="news-app">
@@ -31,6 +41,7 @@ const NewsList = () => {
           description={article.description}
           url={article.url}
           urlToImage={article.urlToImage}
+          onSave={handleSaveArticle} // Pass the onSave function
         />
       ))}
     </div>
